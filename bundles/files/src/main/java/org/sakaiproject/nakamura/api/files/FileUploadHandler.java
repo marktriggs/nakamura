50add2c870ce1522feaa3ef6fb1c8b222b7400b1
Mark Triggs <mark@dishevelled.net>
2011-06-23 16:16:00 +1000

    Added javadoc for the handleFile() method of FileUploadHandler

--- bundles/files/src/main/java/org/sakaiproject/nakamura/api/files/FileUploadHandler.java
+++ bundles/files/src/main/java/org/sakaiproject/nakamura/api/files/FileUploadHandler.java
@@ -22,6 +22,33 @@ import java.io.InputStream;
 import java.util.Map;
 
 public interface FileUploadHandler {
+  /**
+   * This method is called when a file is uploaded via the
+   * CreateContentPoolServlet.  It is called after the file has been added to
+   * the repository, but has the opportunity to add or replace properties by
+   * returning them as a Map.
+   *
+   * @param poolId
+   *          The path of the newly-created content object (a unique identifier).
+   *
+   * @param fileName
+   *          The filename of the uploaded file, as provided by the client.
+   *
+   * @param mimeType
+   *          The mime type of the uploaded file, as provided by the client.
+   *
+   * @param fileInputStream
+   *          A FileInputStream on the uploaded content, set to position zero.
+   *
+   * @param userId
+   *          The name of the logged in client performing the file upload.
+   *
+   * @param isNew
+   *          True if the uploaded file is new content.  False if it replaces an existing node.
+   *
+   * @return A map of properties that will be added to the newly-created content object.
+   *
+   **/
   Map<String, Object> handleFile(String poolId, String fileName, String mimeType, InputStream fileInputStream,
                                  String userId, boolean isNew) throws IOException;
 }
